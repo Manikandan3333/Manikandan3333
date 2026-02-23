ddf0737ddae53062634c4f7edb2dd4b3f22515d6
Manigandan .A <stevekalix04.in@gmail.com>
2026-02-23 21:21:36 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,12 @@
+class sample {
+  public static void main(String []arg){
+    int n=18;
+    if(n>=18){
+      System.out.prinltn("Eligible to vote");
+    }
+    else{
+      System.out.println("Not Eligible to Vote");
+    }
+    
+  }
+}
